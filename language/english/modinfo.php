3ed5b1d87205d6ab8298d5b8c0d3ec5e7ab50980
mambax7 <mambax7@gmail.com>
2015-06-01 06:44:39 -0400

    Adding English

---
+++ language/english/modinfo.php
@@ -0,0 +1,29 @@
+<?php
+include_once XOOPS_ROOT_PATH . "/modules/tadtools/language/{$xoopsConfig['language']}/modinfo_common.php";
+
+define('_MI_TADEVALUA_NAME', 'Tad Evaluation');
+define('_MI_TADEVALUA_AUTHOR', 'Tad');
+define('_MI_TADEVALUA_CREDITS', '');
+define('_MI_TADEVALUA_DESC', 'This module is used to generate a variety of evaluation pages');
+define('_MI_TADEVALUA_AUTHOR_WEB', 'Tad textbook net');
+define('_MI_TADEVALUA_ADMENU1', 'Evaluation management');
+define('_MI_TADEVALUA_ADMENU1_DESC', 'Evaluation management');
+define('_MI_TADEVALUA_BNAME1', 'Evaluation list ');
+define('_MI_TADEVALUA_BDESC1', 'Evaluation List (tad_evaluation_list)');
+define('_MI_TADEVALUA_IGNORED', 'Do not import the file');
+define('_MI_TADEVALUA_IGNORED_DESC', 'do not want to import some system files can be found in this setting, please refer to the separated');
+define('_MI_TADEVALUA_USE_TAB', 'The first layer is set tab index');
+define('_MI_TADEVALUA_USE_TAB_DESC', 'If there are many indicators, a first layer index (directory) to tab');
+define('_MI_TADEVALUA_OS_CHARSET', 'Chinese host file system code');
+define('_MI_TADEVALUA_OS_CHARSET_DESC', 'In general, Windows for Big5, Linux is UTF-8, but there are exceptions, so please set their own uncertain Auto selectable automatic judgment.');
+define('_MI_TADEVALUA_USE_GOOGLE_DOC', 'Use Google Reader to open the file');
+define('_MI_TADEVALUA_USE_GOOGLE_DOC_DESC', 'so you can watch the contents of the file directly online, without downloading');
+define('_MI_TADEVALUA_CSS_SETUP', 'All style settings');
+define('_MI_TADEVALUA_CSS_SETUP_DESC', "Line is a group setting, each line in the format:'level1 {font-size: 12pt; color: blue; ....}. ', the first layer is '\$level1', second layer '\$level2' ... and so on ");
+
+define('_MI_TADEVALUA_DIRNAME', basename(dirname(dirname(__DIR__))));
+define('_MI_TADEVALUA_HELP_HEADER', __DIR__ . '/help/helpheader.html');
+define('_MI_TADEVALUA_BACK_2_ADMIN', 'Back to Administration of ');
+
+//help
+define('_MI_TADEVALUA_HELP_OVERVIEW', 'Overview');
