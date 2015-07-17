3f935de543fcfca6ccfeb638f7b3e2e4e91579bf
Tad <tad0616@gmail.com>
2015-07-17 11:14:13 +0800

    修改程式，使之符合無障礙網頁規範

--- xoops_version.php
+++ xoops_version.php
@@ -4,7 +4,7 @@ $modversion = array();
 
 //---模組基本資訊---//
 $modversion['name']        = _MI_TADEVALUA_NAME;
-$modversion['version']     = '1.8';
+$modversion['version']     = '1.81';
 $modversion['description'] = _MI_TADEVALUA_DESC;
 $modversion['author']      = _MI_TADEVALUA_AUTHOR;
 $modversion['credits']     = _MI_TADEVALUA_CREDITS;
@@ -14,7 +14,7 @@ $modversion['image']       = "images/logo.png";
 $modversion['dirname']     = basename(dirname(__FILE__));
 
 //---模組狀態資訊---//
-$modversion['release_date']        = '2015-07-11';
+$modversion['release_date']        = '2015-07-12';
 $modversion['module_website_url']  = 'http://tad0616.net';
 $modversion['module_website_name'] = _MI_TADEVALUA_AUTHOR_WEB;
 $modversion['module_status']       = 'release';
