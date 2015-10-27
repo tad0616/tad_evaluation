107748188fd8590dfce28353b9433a9c83f75409
Tad <tad0616@gmail.com>
2015-10-27 11:37:05 +0800

    修正tadtools網站路徑

--- language/tchinese/main.php
+++ language/tchinese/main.php
@@ -1,6 +1,6 @@
 <?php
 //需加入模組語系
-define('_TAD_NEED_TADTOOLS', ' 需要 tadtools 模組，可至<a href="http://www.tad0616.net/modules/tad_uploader/index.php?of_cat_sn=50" target="_blank">Tad教材網</a>下載。');
+define('_TAD_NEED_TADTOOLS', '需要 tadtools 模組，可至<a href="http://campus-xoops.tn.edu.tw/modules/tad_modules/index.php?module_sn=1" target="_blank">XOOPS輕鬆架</a>下載。');
 define('_MD_TADEVALUA_SMNAME1', '評鑑列表');
 define('_MD_TADEVALUA_EVALUATION_EMPTY', '尚無檔案，評鑑製作中...');
 
