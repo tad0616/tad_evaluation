7951126c15c687fecb79ab831031b9fd21f198ce
mambax7 <mambax7@gmail.com>
2019-04-10 11:02:47 -0400

    utility
    Signed-off-by: mambax7 <mambax7@gmail.com>

--- include/onInstall.php
+++ include/onInstall.php
@@ -1,25 +1,15 @@
 <?php
 
+use XoopsModules\Tad_evaluation\Utility;
+
+include dirname(__DIR__) . '/preloads/autoloader.php';
+
 function xoops_module_install_tad_evaluation(&$module)
 {
 
-    tad_evaluation_mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_evaluation");
+    Utility::mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_evaluation");
 
     return true;
 }
 
-//建立目錄
-function tad_evaluation_mk_dir($dir = "")
-{
-    //若無目錄名稱秀出警告訊息
-    if (empty($dir)) {
-        return;
-    }
 
-    //若目錄不存在的話建立目錄
-    if (!is_dir($dir)) {
-        umask(000);
-        //若建立失敗秀出警告訊息
-        mkdir($dir, 0777);
-    }
-}
