14e80c2d8f5d30d2245560579decf1c491dff067
Tad <tad0616@gmail.com>
2020-01-03 16:32:15 +0800

    修正無法打包匯出的問題

--- admin/zip.php
+++ admin/zip.php
@@ -1,8 +1,9 @@
 <?php
+
 require_once __DIR__ . '/header.php';
 require_once dirname(__DIR__) . '/function.php';
 
-$evaluation_sn = (int)$_GET['evaluation_sn'];
+$evaluation_sn = (int) $_GET['evaluation_sn'];
 $evaluation = get_tad_evaluation($evaluation_sn);
 $dirname = $evaluation['evaluation_title'];
 $dirname = change_charset($dirname, false);
@@ -17,7 +18,7 @@ $msg = shell_exec('zip -r -j ' . XOOPS_ROOT_PATH . "/uploads/tad_evaluation/{$di
 if (file_exists(XOOPS_ROOT_PATH . "/uploads/tad_evaluation/{$dirname}.zip")) {
     header('location:' . XOOPS_URL . "/uploads/tad_evaluation/{$dirname}.zip");
 } else {
-    require_once dirname(__DIR__) . '/class/pclzip.lib.php';
+    require XOOPS_ROOT_PATH . '/modules/tad_evaluation/class/pclzip.lib.php';
     $zipfile = new PclZip(XOOPS_ROOT_PATH . "/uploads/tad_evaluation/{$dirname}.zip");
     $v_list = $zipfile->create($FromDir, PCLZIP_OPT_REMOVE_PATH, XOOPS_ROOT_PATH . '/uploads/tad_evaluation');
 
