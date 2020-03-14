3007fecb00f144b570e9b2ca11055bfdb161aade
tad0616 <tad0616@gmail.com>
2020-03-14 18:34:00 +0800

    針對無障礙 2.0 AA標準進行修正

--- xoops_version.php
+++ xoops_version.php
@@ -4,7 +4,7 @@ $modversion = [];
 
 //---模組基本資訊---//
 $modversion['name'] = _MI_TADEVALUA_NAME;
-$modversion['version'] = '2.22';
+$modversion['version'] = '2.23';
 $modversion['description'] = _MI_TADEVALUA_DESC;
 $modversion['author'] = _MI_TADEVALUA_AUTHOR;
 $modversion['credits'] = _MI_TADEVALUA_CREDITS;
@@ -14,7 +14,7 @@ $modversion['image'] = 'images/logo.png';
 $modversion['dirname'] = basename(__DIR__);
 
 //---模組狀態資訊---//
-$modversion['release_date'] = '2019/05/10';
+$modversion['release_date'] = '2020/03/14';
 $modversion['module_website_url'] = 'https://tad0616.net';
 $modversion['module_website_name'] = _MI_TADEVALUA_AUTHOR_WEB;
 $modversion['module_status'] = 'release';
