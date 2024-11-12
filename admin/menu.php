0dd86fd49a8c2ecc78e9e9de67276fd8eca723ec
tad0616 <tad0616@gmail.com>
2024-11-12 16:21:04 +0800

    簡化後台選單陣列，修改釋出日期

--- admin/menu.php
+++ admin/menu.php
@@ -1,27 +1,13 @@
 <?php
-//  ------------------------------------------------------------------------ //
-// 本模組由 tad 製作
-// 製作日期：2013-10-23
-// $Id:$
-// ------------------------------------------------------------------------- //
-
-$adminmenu = [];
-$i = 1;
-$icon_dir = '2.6' === mb_substr(XOOPS_VERSION, 6, 3) ? '' : 'images/admin/';
-
-$adminmenu[$i]['title'] = _MI_TAD_ADMIN_HOME;
-$adminmenu[$i]['link'] = 'admin/index.php';
-$adminmenu[$i]['desc'] = _MI_TAD_ADMIN_HOME_DESC;
-$adminmenu[$i]['icon'] = 'images/admin/home.png';
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TADEVALUA_ADMENU1;
-$adminmenu[$i]['link'] = 'admin/main.php';
-$adminmenu[$i]['desc'] = _MI_TADEVALUA_ADMENU1_DESC;
-$adminmenu[$i]['icon'] = "{$icon_dir}Status-mail-task-icon.png";
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TAD_ADMIN_ABOUT;
-$adminmenu[$i]['link'] = 'admin/about.php';
-$adminmenu[$i]['desc'] = _MI_TAD_ADMIN_ABOUT_DESC;
-$adminmenu[$i]['icon'] = 'images/admin/about.png';
+$adminmenu = [
+    [
+        'title' => _MI_TADEVALUA_ADMENU1,
+        'link' => 'admin/main.php',
+        'icon' => "images/admin/Status-mail-task-icon.png",
+    ],
+    [
+        'title' => _MI_TAD_ADMIN_ABOUT,
+        'link' => 'admin/about.php',
+        'icon' => 'images/admin/about.png',
+    ],
+];
